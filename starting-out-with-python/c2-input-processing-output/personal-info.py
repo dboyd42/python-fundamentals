489c4f25cd499533ba3d77f88f7de1d365db6ab3
David Boyd <44727721+dboyd42@users.noreply.github.com>
2019-10-06 12:08:57 -0500

    updated for user input

--- starting-out-with-python/c2-input-processing-output/personal-info.py
+++ starting-out-with-python/c2-input-processing-output/personal-info.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 # Copyright 2019 David Boyd, all rights reserved
-
+# Updated: 2019/10/06
 # A program that display personal information
-print('Name:\t\tDavid Boyd')
-print('Address:\t555 ABC St, Austin, TX 55555')
-print('Telephone:\t(555)555-5555')
-print('Major:\t\tComputer Science / Network Admin / CyberSecurity')
+
+name    = input('Name    : ')
+address = input('Address : ')
+city    = input('City    : ')
+state   = input('State   : ')
+zipCode = input('Zip Code: ')
+ph_num  = input('Phone Number : ')
+major   = input('College Major: ')
+
+print('\nPersonal Information')
+print('--------------------')
+print('Name           : ', name)
+print('Address        : ', address)
+print('City, State ZIP:  ', city, ', ', state, ' ', zipCode, sep='')
+print('Phone Number   : ', ph_num)
+print('College Major  : ', major)
